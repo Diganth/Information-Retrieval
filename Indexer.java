207f38a3702e1862ba078f7a1501e0234dd57dd0
Diganth <diganth2004@gmail.com>
2010-09-21 23:08:28 -0700

    Working copy

--- Indexer.java
+++ Indexer.java
@@ -8,10 +8,17 @@ import java.util.Hashtable;
 
 public class Indexer {
 
-	Hashtable<String, String[]> invtidx = new Hashtable<String, String[]>();
-	String[] paragraphs;
-	String[] docID;
+	static Hashtable<String, Integer[]> invtidx = new Hashtable<String, Integer[]>();
+	Hashtable<String, Integer[][]> posidx = new Hashtable<String, Integer[][]>();
+	String[] paragraphs, positions;
+	Integer[] docID;
+	
+	Integer[][] position;
+	Query query; 
+	Gui gui;
+	int docSize;
 
+	
 	public Indexer(String filename) {
 
 		FileInputStream fis;
@@ -31,52 +38,93 @@ public class Indexer {
 		}
 
 		String data = new String(text);
+		docSize = data.length();
 
 		paragraphs = data.split("\r\n\r\n");
+		
+		query = new Query(paragraphs);
 
-		System.out.println("Number of paragraphs of text: " + paragraphs.length + "\n");
+		/*System.out.println("Number of paragraphs of text: " + paragraphs.length + "\n");
 
 		for (int i = 0; i < paragraphs.length; i++) {
 			System.out.println("Paragraph " + (i + 1) + ": " + paragraphs[i] + "\n------------------------\n");
-		}
+		}*/
 
 	}
+
+	
 
 	public void createIndextable(Hashtable temp){
 
 		try{
-
 			for (Enumeration e = temp.keys(); e.hasMoreElements();){
-				int j=0;
-				docID = new String[paragraphs.length];
+
+				int j=0, l=0;
+				/*
+				 * creating an array of Integers to store the location of the paragraphs 
+				 * in which the tokens are found in and also the positions of the 
+				 */
+				docID = new Integer[paragraphs.length];
+				System.out.println(docSize);
+				position = new Integer[paragraphs.length][1000];
 				String str = (String) temp.get( e.nextElement() );
 
-				System.out.println(str);
 				for (int i = 0; i < paragraphs.length; i++) {
 					if(paragraphs[i].contains(str)){
-						docID[j]=String.valueOf(i+1);
-						//System.out.println("Doc ID is : " + docID[j]);
+						docID[j] = i;
 						j+=1;
+						positions =  paragraphs[i].split("\\s+");
+						for (int k = 0; k < positions.length; k++){
+							if (positions[k].contains(str)) { 
+								position[i][l] = k;
+								l+=1;
+							}
+						}
 					}
+					// Adding the token name and the documents in which they are found into a hashtable "invtidx"
+					invtidx.put(str.toLowerCase(), docID);
+					posidx.put(str, position);
 				}
-				// Adding the token name and the documents in which they are found into a hashtable "invtidx"
-				invtidx.put(str, docID);
+
 			}
 
-			for (Enumeration e = invtidx.keys(); e.hasMoreElements();){
+			/*
+			 * Printing the contents of the Inverted Index hashtable and the position index
+			 */
+			/*for (Enumeration e = invtidx.keys(); e.hasMoreElements();){
 
 				String nextElement = (String) e.nextElement();
-				String[] str = invtidx.get( nextElement );
+				Integer[] str = (Integer[])invtidx.get( nextElement );
+				Integer[][] str1 = (Integer[][])posidx.get( nextElement );
+				System.out.print(nextElement);
 				for (int i = 0; i < str.length; i++) {
 					if(str[i] != null){
-						System.out.println("Paragraph in which token \"" + nextElement +"\" is located in is: " + str[i]  );
+						System.out.print( " --> " + str[i]  );
 					}
-
 				}
-			}
+				System.out.println("\n");
+				System.out.println("Token \"" + nextElement + "\" is positioned at ");
+				for ( int i = 0; i< str1.length; i++){
+					for ( int j = 0; j < 600; j++){
+						if (str1[i][j] != null){
+							System.out.println("-->" + str1[i][j] + " in DOCUMENT" + i);
+						}
+					}
+				}
+			}*/
+			javax.swing.SwingUtilities.invokeLater(new Runnable() {
+	            public void run() {
+	                gui.createAndShowGUI();
+	            }
+	        });
+
+			//gui.createAndShowGUI();
+
 		}catch (Exception e){
 			e.printStackTrace();
 		}
 	}
+
+
 
 }
--- Tokenizer.java
+++ Tokenizer.java
@@ -21,66 +21,65 @@ public class Tokenizer {
 	 * @throws FileNotFoundException 
 	 * 
 	 */
-	
 	static Pattern p;
 	static Hashtable words = new Hashtable();
-	static String filename = "E:\\Eclipse-Workspace\\Workspace1\\IR1\\src\\IR1\\sample.txt";
-		
-	 @SuppressWarnings("unchecked")
+	static String filename = "D:\\Eclipse-Workspace\\Workspace1\\IR1\\src\\IR1\\sample.txt";
+
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws FileNotFoundException {
 
-	    FileReader file = new FileReader(filename);
-	    BufferedReader br = new BufferedReader(file);
-	    String storeWordList = "";
-	    try {
-	    	
-	      while ((storeWordList = br.readLine()) != null) {
-	    	  
-	    	  StringTokenizer st = new StringTokenizer(storeWordList);
-	    	  while (st.hasMoreTokens()){
-	    		  //System.out.println(st.nextToken());
-	    		  try
-			      {
-			         p = Pattern.compile ("[\\p{Punct}]"); //matching pattern for punctuation
-			      }
-			      catch (PatternSyntaxException e)
-			      {
-			         System.err.println ("Regex syntax error: " + e.getMessage ());
-			         System.err.println ("Error description: " + e.getDescription ());
-			         System.err.println ("Error index: " + e.getIndex ());
-			         System.err.println ("Erroneous pattern: " + e.getPattern ());
-			         return;
-			      }
-			      String token = st.nextToken().toLowerCase();
-			      Matcher m = p.matcher (token);
-			      if(m.find() == true){
-			    	 while(m.find ()){
-			    		 token=m.replaceAll("");
-			    	 }
-			    	  token=m.replaceAll("");
-			      }
-			      
-			      if(!token.isEmpty()){
-			    	  //System.out.println("text 1 = " + token);
-			    	  words.put(token,token);
-			      }
-		  	  }
-	      }
-	   // dispose all the resources after using them.
-	      br.close();
-	      file.close();
-	      
-	      System.out.println("Contents of hash table:" + words.values());
-	      //Instantiating the class Indexer
-	      Indexer indexer = new Indexer(filename);
-	      
-	      indexer.createIndextable(words);
-	      
-	    } catch (FileNotFoundException e) {
-	      e.printStackTrace();
-	    } catch (IOException e) {
-	      e.printStackTrace();
-	    }
-	  }
+		FileReader file = new FileReader(filename);
+		BufferedReader br = new BufferedReader(file);
+		String storeWordList = "";
+		try {
+
+			while ((storeWordList = br.readLine()) != null) {
+
+				StringTokenizer st = new StringTokenizer(storeWordList);
+				while (st.hasMoreTokens()){
+					//System.out.println(st.nextToken());
+					try
+					{
+						p = Pattern.compile ("[\\p{Punct}]"); //matching pattern for punctuation
+					}
+					catch (PatternSyntaxException e)
+					{
+						System.err.println ("Regex syntax error: " + e.getMessage ());
+						System.err.println ("Error description: " + e.getDescription ());
+						System.err.println ("Error index: " + e.getIndex ());
+						System.err.println ("Erroneous pattern: " + e.getPattern ());
+						return;
+					}
+					String token = st.nextToken();
+					Matcher m = p.matcher (token);
+					if(m.find() == true){
+						while(m.find ()){
+							token=m.replaceAll("");
+						}
+						token=m.replaceAll("");
+					}
+
+					if(!token.isEmpty()){
+						//System.out.println("text 1 = " + token);
+						words.put(token,token);
+					}
+				}  	  
+			}
+			// dispose all the resources after using them.
+			br.close();
+			file.close();
+
+			System.out.println("Contents of hash table:" + words.values());
+			//Instantiating the class Indexer
+			Indexer indexer = new Indexer(filename);
+
+			indexer.createIndextable(words);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
 
